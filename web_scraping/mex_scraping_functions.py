230576051334d91309908b1692803db66d9ea1b5
Roberto Barroso-Luque <robalu94@gmail.com>
2021-04-29 22:06:24 -0500

    Added el universal scraper

--- web_scraping/mex_scraping_functions.py
+++ web_scraping/mex_scraping_functions.py
@@ -8,6 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from htmldate import find_date
 import datetime
 from web_scraping.get_information import extract_article_info
+import json
 
 
 def selenium_driver_helper(main_url):
@@ -24,6 +25,14 @@ def selenium_driver_helper(main_url):
     return driver
 
 
+def click_next_page(driver, xpath_string):
+    for button in driver.find_elements_by_xpath(xpath_string):
+        try:
+            button.click()
+        except StaleElementReferenceException:
+            continue
+
+
 def crawl_el_universal(keyword):
     """
     Crawler function to use for El Universal search website to get relevant articles about
@@ -31,37 +40,45 @@ def crawl_el_universal(keyword):
     :param keyword: string
     :return all_links: list of links
     """
-    main_url = "https://activo.eluniversal.com.mx/historico/search/index.php?q=" + keyword
-
-    driver = selenium_driver_helper(main_url)
-
     date_now = datetime.date.today()
     date_end = datetime.date(2020, 1, 1)
 
     all_links = []
-    while date_now > date_end:
-        soup1 = BeautifulSoup(driver.page_source, 'html.parser')
-        rows1 = soup1.find_all("a", target="_new")
-        article_links = [x.get("href") for x in rows1]
-        all_links.extend(article_links)
+    for year in ['2021', '2020']:
+        print(f"Working on year {year}")
+        main_url = ("https://activo.eluniversal.com.mx/historico/search/index.php?q="
+                    + keyword + "&anio=" + year + "&editor=&tipo_contenido=articulo")
+        driver = selenium_driver_helper(main_url)
 
-        print(f"Working on date: {date_now}")
-        print(f"Number of links processed so far: {len(all_links)}")
+        while True: # date_now > date_end:
+            soup1 = BeautifulSoup(driver.page_source, 'html.parser')
+            rows1 = soup1.find_all("div", class_="HeadNota")
 
-        date_now = pd.to_datetime(find_date(article_links[-1]))
-
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        for button in driver.find_elements_by_xpath('.//div[@class = "Siguiente btnes"]'):
             try:
-                button.click()
-            except StaleElementReferenceException:
+                article_links = [x.find('a')["href"] for x in rows1]
+            except TypeError:
+                click_next_page(driver, './/div[@class = "Siguiente btnes"]')
                 continue
 
-        # Check if this is the last page, if so break
-        if not soup1.find_all('div', class_="Siguiente btnes"):
-            break
-        time.sleep(1)
+            all_links.extend(article_links)
+
+            print(f"Working on date: {date_now}")
+            print(f"Number of links processed so far: {len(all_links)}")
+
+            with open('list_of_links.json', 'w') as fp:
+                json.dump(all_links, fp)
+            print()
+
+            # date_now = pd.to_datetime(find_date(article_links[-1]))
+
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            click_next_page(driver, './/div[@class = "Siguiente btnes"]')
+
+            # Check if this is the last page, if so break
+            if not soup1.find_all('div', class_="Siguiente btnes"):
+                break
+            time.sleep(1)
 
     return all_links
 
@@ -93,8 +110,7 @@ def crawl_la_jornada(keyword):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(1)
 
-        for button in driver.find_elements_by_xpath('.//span[@class = "next"]'):
-            button.click()
+        click_next_page(driver, './/span[@class = "next"]')
 
         # Check if this is the last page, if so break
         if not soup1.find_all('span', class_="next"):
@@ -107,7 +123,7 @@ def crawl_la_jornada(keyword):
 
 
 if __name__ == '__main__':
-    keyword_list = ["violación+sexual", "violencia+genero", "asesinato+mujer", "matrimonio+forzado",
+    keyword_list = ["violencia+genero", "asesinato+mujer","matrimonio+forzado",
                     "aborto+forzado", "agresion+sexual", "violencia+domestica", "abuso+sexual", "feminicidio"]
 
     # choose one from ["Universal", "LaJornada"]
@@ -128,7 +144,6 @@ if __name__ == '__main__':
 
     print()
     print("Finished compiling all links")
-    print()
 
     list_of_dictionaries = []
     for each_link in set(full_link_lst):
@@ -136,7 +151,15 @@ if __name__ == '__main__':
 
     print()
     print("Finished extracting news information from all links")
+    with open('processed_articles.json', 'w') as fp:
+        print("Saving list of dictionaries to json file")
+        json.dump(list_of_dictionaries, fp)
     print()
 
     final_df = pd.DataFrame(list_of_dictionaries)
-    final_df.to_excel('la_jornada_scraped.xlsx')
+
+    # Remove time zone specification
+    final_df['datetime'] = final_df['datetime'].apply(lambda dt: dt.replace(tzinfo=None))
+    # Double check all articles are from 2020 or 2021
+    final_df = final_df.loc[final_df.datetime > datetime.datetime(2020, 1, 1)]
+    final_df.to_excel(newspaper+'_scraped.xlsx')
